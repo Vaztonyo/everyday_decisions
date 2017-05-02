689c8da0d15cbfb46096ee7a144dd9d246e0a58e
Vaztonyo <tony.dewet7@gmail.com>
2017-05-02 11:12:36 +0200

    done with first part of everyday decisions

---
+++ everyday_decisions.js
@@ -0,0 +1,27 @@
+var musicGenre = document.querySelectorAll('.genre');
+var submit =  document.querySelector('.submitButton');
+    // alert(musicGenre);
+function Q1(){
+  'use strict';
+
+    submit.addEventListener('click', function(event){
+      var selectedGenre = event.target.value;
+
+      if (selectedGenre === 'Hip-Hop'){
+
+
+      }
+
+      if (selectedGenre === 'House'){
+
+
+      }
+
+      if (selectedGenre === 'RnB'){
+
+
+      }
+      // alert(event.target.value);
+    });
+
+};
